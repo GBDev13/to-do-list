f8b0a849a136abda68fa52edd612e7c93ff13a97
Gabriel Borges <gbdesigns13@gmail.com>
2021-01-17 02:51:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+const checkListRouter = require('./src/routes/checklist');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.use('/checklists', checkListRouter);
+
+app.listen(3000, () => {
+  console.log('Servidor foi iniciado')
+})
